415359afe6a348b6b8a1f2c1ffb485219c0b2dc6
Lucca08 <luccabibianogarcia02novo@gmail.com>
2024-05-10 09:49:39 -0300

    Initial commit

---
+++ src/main/java/com/example/TesteViaCEP/TesteViaCepApplication.java
@@ -0,0 +1,13 @@
+package com.example.TesteViaCEP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteViaCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteViaCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TesteViaCEP/TesteViaCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TesteViaCEP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteViaCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
